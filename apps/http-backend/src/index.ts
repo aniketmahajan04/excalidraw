0be84cdba106f74adbc603e6787a21534e960ab8
Aniket Mahajan <mahajananiket207@gmail.com>
2025-02-01 10:28:26 +0530

    create endpoints

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,9 +1,34 @@
-import  express from "express";
+import express from "express";
+import jwt from "jsonwebtoken";
+import { JWT_SECRET } from "./config";
+import { middleware } from "./middleware";
 
 const app = express();
 
 app.get("/", (req, res) => {
     res.send("hello from http-backend")
-})
+});
+
+app.post("/signup", (req, res) => {
+    
+
+});
+app.post("/signin", (req, res) => {
+
+    const userId = 1;
+    const token = jwt.sign({
+        userId
+    }, JWT_SECRET)
+    res.json({
+        token: token
+    })
+});
+app.post("/room", middleware,(req, res) => {
+
+
+    res.json({
+        roomId: "020"
+    })
+});
 
 app.listen(3001);
--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -2,8 +2,14 @@ import { WebSocketServer } from "ws";
 
 const wss = new WebSocketServer({ port: 8080 });
 
-wss.on("connection", function connection(ws) {
-
+wss.on("connection", function connection(ws, req) {
+    const url = req.url;
+    if(!url){
+        return;
+    }
+    const queryParams = new URLSearchParams(url.split('?')[1]);
+    const token = queryParams.get('token');
+    
     ws.on("message", function message(data) {
         ws.send("pong")
     })
