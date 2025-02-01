29242bd5248721d4cca5dc168b9839d76ae57b60
Aniket Mahajan <mahajananiket207@gmail.com>
2025-02-01 22:38:15 +0530

    create signin endpoint and test middleware

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -3,6 +3,8 @@ import jwt from "jsonwebtoken";
 import { JWT_SECRET } from "@repo/backend-common/config";
 import { middleware } from "./middleware";
 import {CreateUserSchema, SignInUserSchema, CreateRoomSchema} from "@repo/common/types";
+import { prismaClient } from "@repo/db/client";
+
 
 const app = express();
 app.use(express.json());
@@ -11,27 +13,52 @@ app.get("/", (req, res) => {
     res.send("hello from http-backend")
 });
 
-app.post("/signup", (req, res) => {
-    const data = CreateUserSchema.safeParse(req.body);
-    if(!data.success){
+app.post("/signup", async (req, res) => {
+    try {
+        const parseData = CreateUserSchema.safeParse(req.body);
+        if(!parseData.success){
+            res.json({
+                msg: "Incorrect format",
+                data: parseData.error
+            });
+            return;
+        }
+    
+        const user = await prismaClient.user.create({
+            data: {
+                email: parseData.data.email,
+                password: parseData.data.password,
+                name: parseData.data.username
+            }
+        })
+        res.json({
+            user
+        })
+    } catch(error){
+        res.status(500).json({
+            msg: "Internal sever error"
+        })
+    }
+
+});
+app.post("/signin", async (req, res) => {
+
+    const parseData = SignInUserSchema.safeParse(req.body);
+    if(!parseData.success){
         res.json({
             msg: "Incorrect format",
-            data: data.error
+            data: parseData.error
         });
         return;
     }
-    res.json({
-        email: data.data.email,
-        username: data.data.username,
-        password: data.data.password
+    const user = await prismaClient.user.findUnique({
+        where: {
+            email: parseData.data.email,
+            password: parseData.data.password
+        }
     })
-
-});
-app.post("/signin", (req, res) => {
-
-    const userId = 1;
     const token = jwt.sign({
-        userId
+        userId: user?.id
     }, JWT_SECRET)
     res.json({
         token: token
