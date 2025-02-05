50c58374baca4cf4e1278d85dd84adf50fe34bfd
Aniket Mahajan <mahajananiket207@gmail.com>
2025-02-05 18:55:00 +0530

    created first shape

---
+++ apps/ex-frontend/app/canvas/[roomId]/page.tsx
@@ -0,0 +1,49 @@
+"use client"
+
+import { Rye } from "next/font/google";
+import { startTransition, useEffect, useRef } from "react";
+
+export default function Canvas() {
+    const canvasRef = useRef<HTMLCanvasElement>(null);
+
+    let clicked = false;
+    let startX = 0;
+    let startY = 0;
+
+    useEffect(() => {
+        if(canvasRef.current) {
+            const canvas = canvasRef.current;
+            const ctx = canvas.getContext("2d");
+            
+            if(!ctx) return;
+
+            canvas.addEventListener("mousedown", (e) => {
+                clicked = true;
+                startX = e.clientX;
+                startY = e.clientY;
+            })
+
+            canvas.addEventListener("mousemove", (e) => {
+                if(clicked) {
+                    const width = e.clientX - startX;
+                    const height = e.clientY - startY;
+                    ctx.clearRect(0, 0, canvas.width, canvas.height);
+                    ctx.strokeRect(startX, startY, width, height)
+                }
+            })
+
+            canvas.addEventListener("mouseup", () => {
+                clicked = false;
+            })
+
+        }
+    }, [])
+
+    
+
+    
+
+    return <div>
+        <canvas ref={canvasRef} width={500} height={500}></canvas>
+    </div>
+}
